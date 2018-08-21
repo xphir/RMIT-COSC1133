25a1a2ab226d0d819fd8c8d3d1b7fd23351c78a1
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-21 22:41:49 +1000

    fix services

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 # Put any tasks you would like to have carried
 # out when the container is first created here
-service ssh start
-service nginx start
+/etc/init.d/ssh start
+/etc/init.d/nginx start
