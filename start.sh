97d7c273af499f51f0c8ac35d5804ac66f378b32
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-21 22:48:54 +1000

    wip

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 # Put any tasks you would like to have carried
 # out when the container is first created here
+echo "mrfishy:cosc1133" | chpasswd
 /etc/init.d/ssh start
 /etc/init.d/nginx start
-echo "mrfishy:cosc1133" | chpasswd
