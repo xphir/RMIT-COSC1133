d36278ebcc5d3acec42a920f30b2b0e4ee2cc91d
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-22 00:26:13 +1000

    wip

--- start.sh
+++ start.sh
@@ -2,13 +2,14 @@
 
 # Put any tasks you would like to have carried
 # out when the container is first created here
-/etc/init.d/ssh start
-/etc/init.d/nginx start
 
 #ROOT_PASSWORD=`pwgen -c -n -1 12`
 PASSWORD='assignment'
 #echo "mrfishy:$PASSWORD" | chpasswd
 echo -e "$PASSWORD\n$PASSWORD" | passwd mrfishy
 echo "mrfishy login password: $PASSWORD"
+
+/etc/init.d/ssh start
+/etc/init.d/nginx start
 
 while true; do sleep 1000; done
